9db9124d737d576f16738a09222eab26607cfd08
Davi Israel Quirino <davi.iq@aluno.ifsc.edu.br>
2026-02-24 22:15:24 -0300

    Inicio do projeto da implementação da IA para leitura de Emails. Criado 2 classes iniciais simples

---
+++ Domain/Email.java
@@ -0,0 +1,7 @@
+public class Email {
+    private String remetente;
+    private String assunto;
+    private String destinatario;
+    private String status;
+
+}
---
+++ Domain/Usuario.java
@@ -0,0 +1,20 @@
+public class Usuario {
+
+    //Atributos:
+    private String nome;
+    private String email;
+    private String matricula;
+
+    //Constructor:
+    public Usuario(String nome, String email, String matricula) {
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+    }
+
+    //Pega o email e determina como deve ser:
+    public String getEmail() {return email;}
+    public String setEmail(String formato) {
+        return String.format("%s" + "@intelbras.com.br", matricula);
+    }
+}
