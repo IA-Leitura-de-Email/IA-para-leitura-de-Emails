378dd3cf6528e0bb423d01cd69830683c0dc0b5d
Davi Israel Quirino <da059230@intelbras.com.br>
2026-03-04 13:56:36 -0300

    Integração do Maven para otimização de tarefas, e permitir leituras de PDFs

--- Src/Domain/KnowledgeEntry.java
+++ Src/Domain/KnowledgeEntry.java
@@ -1,7 +1,18 @@
 import Enums.KnowledgeType;
 import java.io.File;
 import java.time.LocalDateTime;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
+/*O Maven é tipo o gradle, ele serve para
+ Gerenciamento de dependências
+ Build do projeto
+ Padronização de estrutura
+
+ Sem o Maven, seria necessário baixar os arquivos para leituras
+ de PDF manualmente. Com o Maven não é preciso, ele já faz este
+ procedimento sozinho
+ */
 
 import java.util.List;
 
